b0876575515d38cb178448e05ac7383de634afa5
Harlan <harlan@dragon.local>
2018-04-07 21:55:23 -1000

    init: mix new w/ elixir 1.6.4

---
+++ lib/qualitymatrix.ex
@@ -0,0 +1,18 @@
+defmodule Qualitymatrix do
+  @moduledoc """
+  Documentation for Qualitymatrix.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Qualitymatrix.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/qualitymatrix_test.exs
@@ -0,0 +1,8 @@
+defmodule QualitymatrixTest do
+  use ExUnit.Case
+  doctest Qualitymatrix
+
+  test "greets the world" do
+    assert Qualitymatrix.hello() == :world
+  end
+end
