3c76a9484ba1d75e7d14ee3d6211da6260236746
Harlan T Wood <code@harlantwood.net>
2021-06-26 13:44:36 -1000

    Github repo data

--- src/routes/index.json.ts
+++ src/routes/index.json.ts
@@ -2,18 +2,33 @@ import { Octokit, App, Action } from "octokit";
 
 export async function get() {
 
-  const repoFullNames = ['holochain/holochain', 'holochain/blarg']
+  const octokit = new Octokit({})
+  const repoFullNames = ['holochain/holochain',
+    'holochain/holochain-dna-build-tutorial']
 
   // const repos = repoFullNames.map((repoFullName) => {
   //   // const owner, repo = repoFullName.split('/')
   //   return { owner, repo }
   // })
 
-  const octokit = new Octokit({})
+  const repoPromises = repoFullNames.map((repoFullName) => {
+    const [owner, repo] = repoFullName.split('/')
+    return octokit.rest.repos.get({ owner, repo })
+  })
+
+
+  const repoResponses = await Promise.allSettled(repoPromises)
+  // console.log(repoResponses)
+  const reposData = repoResponses.map((repoResponse) => repoResponse.value.data)
+  // console.log(reposData)
+
+  // const repos = Promise.all(repoFullNames).then((repos) => {
+  // })
+
   // console.log(octokit)
   // octokit.rest.actions.listRepoWorkflows({owner, repo})
   const wfs = await octokit.rest.actions.listRepoWorkflows({ owner: 'holochain', repo: 'holochain' })
-  console.log(wfs.data.workflows)
+  // console.log(wfs.data.workflows)
 
 
   return {
