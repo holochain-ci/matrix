252a284784f14531cf575621da29e7bc7370c545
Harlan T Wood <code@harlantwood.net>
2021-06-26 17:09:20 -1000

    Build to pure static assets

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+// /** @type {import('@sveltejs/kit').Config} */
+
+import adapter from '@sveltejs/adapter-static';
+
+export default {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
 		target: '#svelte'
 	}
 };
-
-export default config;
