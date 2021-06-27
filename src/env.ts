77390af8d4f6449a3f6b2ec0dfedf001617ee4aa
Harlan T Wood <code@harlantwood.net>
2021-06-26 14:52:22 -1000

    Load data from github w/ auth

---
+++ src/env.ts
@@ -0,0 +1,1 @@
+export const GITHUB_ACCESS_TOKEN = import.meta.env.VITE_GITHUB_ACCESS_TOKEN
--- src/routes/index.json.ts
+++ src/routes/index.json.ts
@@ -1,8 +1,18 @@
-import { Octokit, App, Action } from "octokit";
+// import 'dotenv/config'
+
+// import dotenv from 'dotenv'
+// dotenv.config()
+
+import { Octokit } from "octokit";
+
+import { GITHUB_ACCESS_TOKEN } from '../env'
 
 export async function get() {
 
-  const octokit = new Octokit({})
+  console.log({ auth: GITHUB_ACCESS_TOKEN })
+  console.log({ auth: GITHUB_ACCESS_TOKEN })
+  console.log({ auth: GITHUB_ACCESS_TOKEN })
+  const octokit = new Octokit({ auth: GITHUB_ACCESS_TOKEN })
   const repoFullNames = ['holochain/holochain',
     'holochain/holochain-dna-build-tutorial']
 
@@ -11,22 +21,17 @@ export async function get() {
     return octokit.rest.repos.get({ owner, repo })
   })
   const repoResponses = await Promise.all(repoPromises)
-  console.log(repoResponses)
-  const repos = repoResponses.map((repoResponse) => repoResponse.data)
+  // console.log(repoResponses)
+  let repos = repoResponses.map((repoResponse) => repoResponse.data)
 
-  // const workflowPromises = repos.map((repo) => {
-  //   return octokit.rest.actions.listRepoWorkflows({ owner: repo.owner.login, repo: repo.name })
-  // })
-  // const workFlowPromisesFulfilled = await Promise.all(workflowPromises)
-  // const workflows = repoResponses.map((repoResponse) => repoResponse.value.data)
-
-  repos.map((repo) => {
-    octokit.rest.actions.listRepoWorkflows({ owner: repo.owner.login, repo: repo.name })
-      .then((workflows) => {
-        repo.workflows = workflows.data.workflows
-        console.log(repo)
-      })
+  const workflowPromises = repos.map((repo) => {
+    return octokit.rest.actions.listRepoWorkflows({ owner: repo.owner.login, repo: repo.name })
   })
+  const workFlowPromisesFulfilled = await Promise.all(workflowPromises)
+
+  for (let i = 0; i < repos.length; i++) {
+    repos[i].workflows = workFlowPromisesFulfilled[i].data
+  }
 
   return {
     body: {
