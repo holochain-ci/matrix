1c33346aed453fb57ddf02dbd4ed3aa6692e6559
Harlan T Wood <code@harlantwood.net>
2021-06-28 00:11:25 -1000

    Dotenv FTW

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,1 +1,10 @@
-export const GITHUB_ACCESS_TOKEN = import.meta.env.VITE_GITHUB_ACCESS_TOKEN
+import { assertExists } from "./assert"
+
+import dotenv from 'dotenv'
+const dotenvResult = dotenv.config()
+if (dotenvResult.error) {
+  throw dotenvResult.error
+}
+
+assertExists(process.env['GITHUB_ACCESS_TOKEN'])
+export const GITHUB_ACCESS_TOKEN = process.env['GITHUB_ACCESS_TOKEN']
