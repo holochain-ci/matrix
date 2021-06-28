e025b5ebdd6e6ce7f4e09d04f70a2d7a207929a3
Harlan T Wood <code@harlantwood.net>
2021-06-27 21:52:01 -1000

    Remove problematic svelte-table examples

--- src/routes/svelte-table-examples/example6/ContactButtonComponent.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  export let row
-  export let onContactButtonClick
-  export let col
-  let key = col.key.charAt(0).toUpperCase() + col.key.slice(1)
-</script>
-
-<button on:click={() => onContactButtonClick(row)}>{key} {row.first_name} {row.last_name} </button>
--- src/routes/svelte-table-examples/example6/HelpButtonComponent.svelte
+++
@@ -1,4 +0,0 @@
-<button
-  on:click={() => alert('Read the svelte-table docs: https://github.com/dasDaniel/svelte-table')}
-  >Help</button
->
