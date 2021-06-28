afde48e09727db0c0ef7f49cb5c9ee616836f861
Harlan T Wood <code@harlantwood.net>
2021-06-28 00:19:18 -1000

    Rename env var

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,5 +3,5 @@ import { assertExists } from "./assert"
 import dotenv from 'dotenv'
 dotenv.config()
 
-assertExists(process.env['GITHUB_ACCESS_TOKEN'])
-export const GITHUB_ACCESS_TOKEN = process.env['GITHUB_ACCESS_TOKEN']
+assertExists(process.env['PUBLIC_REPOS_GITHUB_ACCESS_TOKEN'])
+export const GITHUB_ACCESS_TOKEN = process.env['PUBLIC_REPOS_GITHUB_ACCESS_TOKEN']
