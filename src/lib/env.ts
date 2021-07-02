11e524a0d76341de9a3db5cfc57a32876fe3b2e9
Harlan T Wood <code@harlantwood.net>
2021-07-01 21:22:54 -1000

    Switch to dotenv

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,1 +1,10 @@
-export const GITHUB_ACCESS_TOKEN = import.meta.env.VITE_GITHUB_ACCESS_TOKEN
+import { assertExists } from './assert'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+assertExists(
+  process.env['PUBLIC_REPOS_GITHUB_ACCESS_TOKEN'],
+  "process.env['PUBLIC_REPOS_GITHUB_ACCESS_TOKEN']"
+)
+export const GITHUB_ACCESS_TOKEN = process.env['PUBLIC_REPOS_GITHUB_ACCESS_TOKEN']
