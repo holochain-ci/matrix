a3107e5369ad9eeba05d03ced55e4455ae6463ab
Harlan T Wood <code@harlantwood.net>
2021-07-01 21:00:57 -1000

    Back to VITE_GITHUB_ACCESS_TOKEN

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,10 +1,9 @@
 import { assertExists } from './assert'
 
-import dotenv from 'dotenv'
-dotenv.config()
 
 assertExists(
-  process.env['PUBLIC_REPOS_GITHUB_ACCESS_TOKEN'],
-  "process.env['PUBLIC_REPOS_GITHUB_ACCESS_TOKEN']"
+  import.meta.env.VITE_GITHUB_ACCESS_TOKEN,
+  "import.meta.env.VITE_GITHUB_ACCESS_TOKEN"
 )
-export const GITHUB_ACCESS_TOKEN = process.env['PUBLIC_REPOS_GITHUB_ACCESS_TOKEN']
+
+export const GITHUB_ACCESS_TOKEN = import.meta.env.VITE_GITHUB_ACCESS_TOKEN
