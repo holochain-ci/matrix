a0172dca5931ea0fcd912155d00d0a894e438ba0
Harlan T Wood <code@harlantwood.net>
2021-07-04 18:16:37 -1000

    Lint: no-console; sort lintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,23 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  plugins: ['svelte3', '@typescript-eslint'],
-  ignorePatterns: ['*.cjs', 'src/routes/svelte-table-examples/**/*'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-  settings: {
-    'svelte3/typescript': () => require('typescript'),
-  },
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2019,
-  },
   env: {
     browser: true,
     es2017: true,
     node: true,
   },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  ignorePatterns: ['*.cjs', 'src/routes/svelte-table-examples/**/*'],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2019,
+  },
+  plugins: ['svelte3', '@typescript-eslint'],
+  settings: {
+    'svelte3/typescript': () => require('typescript'),
+  },
+  rules: {
+    'no-console': 'error',
+  },
+  root: true,
 }
