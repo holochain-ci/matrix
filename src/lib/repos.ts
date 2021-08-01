c26534b3ded05d29cc8819fd51a721ac819d50da
Harlan T Wood <code@harlantwood.net>
2021-08-01 13:30:08 -1000

    Revert much of previous spike

--- src/lib/repos.ts
+++ src/lib/repos.ts
@@ -148,34 +148,3 @@ export function indexHolochainVersions(repos) {
   })
   return repos
 }
-
-type RepoForUi = {
-  default_branch: string
-  full_name: string
-  nix_holochain_version: string
-  nix_holochain_version_date: string
-  pushed_at: string
-  workflows: string
-}
-
-// trim down to the fields needed by interface
-export function fieldsForUi(repos): Array<RepoForUi> {
-  repos = repos.map((repo) => {
-    const workflowData = repo.workflows.map((workflow) => {
-      return {
-        badge_url: workflow.badge_url,
-        path: workflow.path,
-      }
-    })
-    return {
-      default_branch: repo.default_branch,
-      full_name: repo.full_name,
-      nix_holochain_version: repo.nix_holochain_version,
-      nix_holochain_version_date: repo.nix_holochain_version_date,
-      nix_holochain_version_date_index: repo.nix_holochain_version_date_index,
-      pushed_at: repo.pushed_at,
-      workflows: workflowData,
-    }
-  })
-  return repos
-}
